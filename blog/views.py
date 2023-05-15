5378a2dc1507e8bf2212ca29a58c39c9c1a1edaf
pillow3 <rawraul@outlook.com>
2023-05-15 10:39:56 +0100

    Adding the All Posts Page & Style

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    return render(request, 'blog/index.html')
+
+def posts(request):
+    return render(request, 'blog/all-posts.html')
+    
+
+def post_detail(request):
+    pass
