ac7da276f9ef80b21dd539de7b70fc3904625076
pillow3 <rawraul@outlook.com>
2023-05-30 09:46:51 +0100

    127. All Post & Single Post Pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Post, Author
+from .models import Post, Author, Tag
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -5,23 +5,38 @@ from django.urls import reverse
 # Create your models here.
 
 class Author(models.Model):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return f"{self.first_name} {self.last_name}"
 
-    
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.caption
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
     post_content = models.TextField()
-    date = models.DateField()
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True,
+                            default="", null=False, blank=True)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="posts")
+    tags = models.ManyToManyField(Tag)
 
     def get_absolute_url(self):
         return reverse("post-detail-page", args=[self.slug])
-    
+
     def __str__(self) -> str:
         return f"{self.title}"
-    
