25f471a2272366cb1b3abc19dd1743ae965e540d
pillow3 <rawraul@outlook.com>
2023-06-20 07:29:40 +0100

    187. Serving Uploaded Files

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     post_content = models.TextField()
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True,
